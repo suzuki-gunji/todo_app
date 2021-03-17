8470e2bd91abecc543447e75e1ca0b2802dda5e0
suzuki-gunji <gunji1102@gmail.com>
2021-03-17 20:18:30 +0900

    編集機能、削除機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,25 @@ class TasksController < ApplicationController
     end  
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render "edit"  
+    end  
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
   private
   def task_params
     params[:task].permit(:title)
