5a02e5b278d1323f10b158c7518506d4d8487c1b
suzuki-gunji <gunji1102@gmail.com>
2021-03-17 19:59:23 +0900

    新規登録機能追加

--- db/migrate/20210317102329_create_tasks.rb
+++ db/migrate/20210317102329_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :done
+      t.boolean :done, default: false
 
       t.timestamps
     end
